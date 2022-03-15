08e607af2cf205016a2ecba0a5f9dccc9e6f0298
MaksimMistec <maksimmistec@gmail.com>
2022-03-15 01:03:46 +0100

    style: changed some variable names from slovak to english

--- 04_powerstation.py
+++ 04_powerstation.py
@@ -45,20 +45,20 @@ def init():
     get the threads running.
     """
     access_data = Semaphore(1)
-    turniket = Semaphore(1)
+    semaphore = Semaphore(1)
     ls_monitor = Lightswitch()
     ls_sensor = Lightswitch()
     valid_data = Event()
 
     for monitor_id in range(8):
-        Thread(monitor, monitor_id, valid_data, turniket, ls_monitor,
+        Thread(monitor, monitor_id, valid_data, semaphore, ls_monitor,
                access_data)
 
     for sensor_id in range(3):
-        Thread(sensor, sensor_id, turniket, ls_sensor, valid_data, access_data)
+        Thread(sensor, sensor_id, semaphore, ls_sensor, valid_data, access_data)
 
 
-def monitor(monitor_id, valid_data, turniket, ls_monitor, access_data):
+def monitor(monitor_id, valid_data, semaphore, ls_monitor, access_data):
     """
     Function used to manage monitors. They wait until all sensors write
     their part once before they can start monitoring.
@@ -68,34 +68,34 @@ def monitor(monitor_id, valid_data, turniket, ls_monitor, access_data):
     while True:
         reading_length = randint(40, 50) / 1000
         sleep(reading_length)
-        turniket.wait()
-        pocet_citajucich_monitorov = ls_monitor.lock(access_data)
-        turniket.signal()
+        semaphore.wait()
+        number_of_reading_monitors = ls_monitor.lock(access_data)
+        semaphore.signal()
 
         print(f'monit "{monitor_id:02d}": '
-              f'pocet_citajucich_monitorov={pocet_citajucich_monitorov:02d},'
+              f'number_of_reading_monitors={number_of_reading_monitors:02d},'
               f'reading_length={reading_length:5.3f}')
         ls_monitor.unlock(access_data)
 
 
-def sensor(sensor_id, turniket, ls_sensor, valid_data, access_data):
+def sensor(sensor_id, semaphore, ls_sensor, valid_data, access_data):
     """
     Function used to write imaginary data that monitors will then read.
     """
     while True:
         sleep(randint(50, 60)/1000)
-        turniket.wait()
-        turniket.signal()
+        semaphore.wait()
+        semaphore.signal()
 
-        pocet_zapisujucich_cidiel = ls_sensor.lock(access_data)
+        number_of_writing_sensors = ls_sensor.lock(access_data)
         if sensor_id == 2:
-            trvanie_zapisu = randint(20, 25) / 1000
+            writing_length = randint(20, 25) / 1000
         else:
-            trvanie_zapisu = randint(10, 20) / 1000
+            writing_length = randint(10, 20) / 1000
         print(f'sensor "{sensor_id:02d}":  '
-              f'pocet_zapisujucich_cidiel={pocet_zapisujucich_cidiel:02d}, '
-              f'trvanie_zapisu={trvanie_zapisu:5.3f}')
-        sleep(trvanie_zapisu)
+              f'number_of_writing_sensors={number_of_writing_sensors:02d}, '
+              f'writing_length={writing_length:5.3f}')
+        sleep(writing_length)
         valid_data.signal()
         ls_sensor.unlock(access_data)
 
